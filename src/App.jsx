82876c20d934cf183cb67e0a40a4670b114498f1
VIVEK KUMAR <yvksharma@gmail.com>
2024-03-08 18:46:24 +0530

    Addition

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import ImgBanner from "./components/imgBanner";
+
+function App() {
+  return (
+    <div className="tile-container">
+      <ImgBanner />
+      <ImgBanner />
+      <ImgBanner />
+      <ImgBanner />
+      <ImgBanner />
+      <ImgBanner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/imgBanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ImgBanner = () => {
+  return (
+    <div>
+      <div className="container">
+        <img src="/dcp.jpg" alt="camera"  />
+        <div className="text-block">
+          <h4>Nature</h4>
+          <p>What a beautiful sunrise</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ImgBanner
